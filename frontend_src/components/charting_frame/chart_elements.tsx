694fadc8fc6b9ab2cc81aa4e444e4e99d11f8299
Bryce Hawk <Bryce.m.hawk@gmail.com>
2024-09-06 13:56:11 -0500

    Charting Pane Scale Button decluttering

--- frontend_src/components/charting_frame/chart_elements.tsx
+++ frontend_src/components/charting_frame/chart_elements.tsx
@@ -57,13 +57,13 @@ export function ChartPane(props:chart_pane_props){
         {props.chart_el}
         <PaneLegend {...props.legend_props}/>
         <ScaleToggle 
-            class='scale_buttons_left'
+            right={false}
             scale_ref={left_scale_el} 
             scale_signal={props.leftScaleMode} 
             invert_signal={props.leftScaleInvert} 
         />
         <ScaleToggle
-            class='scale_buttons_right'
+            right={true}
             scale_ref={right_scale_el} 
             scale_signal={props.rightScaleMode} 
             invert_signal={props.rightScaleInvert} 
@@ -79,6 +79,7 @@ export function ChartPane(props:chart_pane_props){
  * index-to-100 scales. The scale can also be inverted.
  */
 interface scale_props extends JSX.HTMLAttributes<HTMLDivElement>{
+    right:boolean
     scale_ref:HTMLTableCellElement
     scale_signal:Signal<number>, 
     invert_signal:Signal<boolean>
@@ -86,7 +87,7 @@ interface scale_props extends JSX.HTMLAttributes<HTMLDivElement>{
 function ScaleToggle(props:scale_props){
     let divRef = document.createElement('div')
     const [show, setShow] = createSignal(false)
-    const [wrapperStyle, setWrapperStyle] = createSignal<JSX.CSSProperties>({width:'0px'})
+    const [wrapperStyle, setWrapperStyle] = createSignal<JSX.CSSProperties>({})
 
     const event_cleaner = new AbortController()
     onCleanup(()=>{event_cleaner.abort()})
@@ -97,24 +98,24 @@ function ScaleToggle(props:scale_props){
 
     props.scale_ref.addEventListener('mouseenter', ()=>{
         const rect = props.scale_ref.getBoundingClientRect()
-        setWrapperStyle({ width:`${Math.min(rect.width/2), 28}px`})
+        const pos = rect.width/2 - 14
+        setWrapperStyle(props.right? {right:`${pos}px`} : {left:`${pos}px`})
         setShow(true)
     },{signal:event_cleaner.signal})
 
     //Set to Mode if not already, otherwise reset to normal
-    const setMode = (mode:number) => {
-        props.scale_signal[1](props.scale_signal[0]() !== mode? mode : 0)
-    }
+    const setMode = (mode:number) => props.scale_signal[1](props.scale_signal[0]() !== mode? mode : 0)
 
+    // percent and indexd to 100 aren't as common. Commented out for now since they clutter the UI.
     return <Show when={show()}>
-        <div ref={divRef} class={props.class} style={wrapperStyle()}>
+        <div ref={divRef} class={props.right?'scale_buttons_right':'scale_buttons_left'} style={wrapperStyle()}>
             <TextIcon 
                 text={"L"}
                 onClick={()=>setMode(1)}
                 activated={props.scale_signal[0]() === 1}
                 classList={{icon_text:false, scale_icon_text:true}}
             />
-            <TextIcon 
+            {/* <TextIcon 
                 text={"%"} 
                 onClick={()=>setMode(2)}
                 activated={props.scale_signal[0]() === 2}
@@ -125,10 +126,9 @@ function ScaleToggle(props:scale_props){
                 onClick={()=>setMode(3)}
                 activated={props.scale_signal[0]() === 3}
                 classList={{icon_text:false, scale_icon_text:true}}
-            />
+            /> */}
             <TextIcon 
                 text={"I"}
-                style={{'margin-top':'6px'}}
                 activated={props.invert_signal[0]()}
                 onClick={()=>props.invert_signal[1](!props.invert_signal[0]())}
                 classList={{icon_text:false, scale_icon_text:true}}
